55000c8eb67f3703f2ab3e6744c9e824f7d15ee0
CaptainPandaMonkey <70558552+CaptainPandaMonkey5@users.noreply.github.com>
2022-02-06 11:06:54 +0800

    Update main.cpp
    fixed the grammar
    added some lines from the prologue; still not finished cuz mag luluto ako
    gonna finish prologue

--- console ver/main.cpp
+++ console ver/main.cpp
@@ -6,9 +6,11 @@
 using namespace std;
 
 // Intro class of the game
-class Intro{
+class Intro
+{
    public:
-      void run(){
+      void run()
+      {
       	// Title of the game
       	 cout << "------------------------------------------------"<< endl;
          cout << " Dating Simulator made by Group 2 in MCS102 BSU" << endl;
@@ -29,15 +31,20 @@ class Intro{
       	getline(cin, play);
       	while (true)
       	{
-      		if (play == "Y" || play == "y" || play == "yes" || play == "Yes"){
+      		if (play == "Y" || play == "y" || play == "yes" || play == "Yes")
+            {
 
       			return 1;
       			break;
-      		}else if(play == "N" || play == "n" || play == "No" || play == "no"){
+      		}
+            else if(play == "N" || play == "n" || play == "No" || play == "no")
+            {
       			return 0;
       			break;
 
-      		}else{
+      		}
+            else
+            {
       			cout << "Please choose only Y or N: ";
       			// This getline command is like the cin command or scanf command
       			// But this command read all the strings within the line including spaces
@@ -58,11 +65,11 @@ class Prologue{
       // First Scene in the Prologue
 	  int scene1(){
 	  	cout << endl;
-	  	cout << "Its my first day of school and Im running pretty late. This happened because I pulled an all-nighter playing a game." << endl;
+	  	cout << "It's my first day of school and Im running pretty late. This happened because I pulled an all-nighter playing a game." << endl;
          // The sleep command came from the header <windows.h> it is use to delay the output in the console
 	  	Sleep(4000);
 	  	cout << endl;
-	  	cout << "I was grinding for a mega climactic flesh vibrator for my game." << endl;
+	  	cout << "I was grinding for a Dark Iron Sword for my game." << endl;
 	  	cout << endl;
 	  	Sleep(3000);
 	  	cout << "Hopefully I won't be late on the first day of school." << endl;
@@ -71,10 +78,10 @@ class Prologue{
 	  	cout << "*THUMP*"<< endl;
 	  	cout << endl;
 	  	Sleep(3000);
-	  	cout << "[Angel]: Hey! Why did you bumped me?" << endl;
+	  	cout << "[Angel]: Hey! Why did you bump into me?" << endl;
 	  	Sleep(3000);
 	  	cout << endl;
-	  	cout << "Responses:\n[1] Sorry *helps her* \n[2] Im so sorry *helps and escort her*\n[3] … (...)\n[4] Nag over-think"<<endl;
+	  	cout << "Responses:\n[1] Sorry *helps her* \n[2] I'm so sorry *helps and escort her*\n[3] … (...)\n[4] *Start Panicking"<<endl;
 	  	cout << "Option: ";
           
           //get line is the same as cin and scanf the difference is that getline reads the whole line
@@ -103,10 +110,10 @@ class Prologue{
 	  		Sleep(3000);
 	  		cout << "*I Escorted her to her class*"<< endl << endl;
 	  		Sleep(3000);
-	  		cout << "[Angel]: Thanks for escorting me\n\n*On class*\n" << endl;
+	  		cout << "[Angel]: Thanks for escorting me\n\n*In class*\n" << endl;
 	  		Sleep(3000);
 	  		cout << "Inner monologue" << endl;
-	  		cout << "[MC]: Damn that women I just bumped looked fine af\n" << endl;
+	  		cout << "[MC]: Damn, that woman I just bumped looked gorgeous.\n" << endl;
 	  		Sleep(3000);
 	  		break;	
 	  	}
@@ -133,28 +140,28 @@ class Prologue{
 	  }
     
       // Second Scene in the Prologue
-    // Almost the smae as the first scne
+    // Almost the same as the first scene
     // compose of cout, cin, Sleep, while and if and else statements.
       void scene2()
       {
       	Sleep(3000);
-      	cout << "*Bell rings* -- Launch Break\n"<< endl;
+      	cout << "*Bell rings* -- Lunch Break\n"<< endl;
       	Sleep(3000);
       	cout << "Inner Monologue" << endl;
-      	cout << "[MC]: *moan* finally its lunch time, time to go to the cafeteria.\n"<< endl;
+      	cout << "[MC]: *groans* Finally, it's lunch time, time to go to the cafeteria.\n"<< endl;
       	Sleep(3000);
-      	cout << "[MC]: *ugh* finally after a while lining up in this line I can finally order"<< endl << endl;
+      	cout << "[MC]: *ugh* Eventually, after a while lining up in this line, I can now order."<< endl << endl;
       	Sleep(3000);
       	cout << "Waitress: What do you want?!" << endl << endl;
       	Sleep(3000);
       	cout << "Inner Monologue" << endl;
-      	cout << "[MC]: Suddenly from my back I felt a sudden force.\n" << endl;
+      	cout << "[MC]: Suddenly, from my back I felt a sudden force.\n" << endl;
       	Sleep(2000);
       	cout <<"*Thump*" <<endl <<endl;
       	Sleep(3000);
-      	cout << "[Mc]: Stupid Bitch bumped me\n"<< endl;
+      	cout << "[Mc]: Stupid bastard bumped me.\n"<< endl;
       	Sleep(3000);
-      	cout << "Responses:\n[1] Be angry ( Temporary)\n[2] Let her be\n[3] Suntukin pader\n[4] Suntukin ung babae (domestic violence route) (chad move)\n" << endl;
+      	cout << "Responses:\n[1] Be angry ( Temporary)\n[2] Let her be\n" << endl;
       	cout << "Option: ";
       	getline(cin, None);
       	
@@ -163,6 +170,14 @@ class Prologue{
       	{
       	if(None == "1")
       	{	
+            Sleep(3000);
+            cout << "[???]: Hey what's with you?!\n" << endl;
+            Sleep(3000);
+            cout << "[MC]: What do you mean? I got here first. There is a line, stupid.\n" << endl;
+            Sleep(3000);
+            cout << "[???]: So what? I'm hungry, mind your own business." << endl;
+            Sleep(3000);
+            cout << "[MC]: Do you think you're the only one hungry here?" << endl;
       		break;
       	}
       	else if(None == "2")	
@@ -183,6 +198,32 @@ class Prologue{
       	}
       	
       	}
+
+        // Third Scene End of classes
+
+        void scene3()
+        {
+            Sleep(3000);
+            cout << "*Bell rings\n" << endl;
+            cout << "*End of the day\n" << endl;
+            Sleep(3000);
+            cout << "Inner Monologue" << endl;
+            cout << "[MC]: I don't wanna go home; it's not like there's anyone at home anyways.\n" << endl;
+            cout << "*after a while I fell asleep\n" << endl;
+            Sleep(3000);
+            cout << "[???]: Hey, wake up...\n" << endl;
+            Sleep(3000);
+            cout << "ugdgjaksdjie, who is that?\n" << endl;
+            Sleep(3000);
+            cout << "[???]: Hey kōhai wake up, It's getting late.\n" << endl;
+            Sleep(3000);
+            cout << "[MC]: Ugh, who are you?\n" << endl;
+            Sleep(3000);
+            cout << "Inner Monologue\n" << endl;
+            cout << "(I can't clearly see her face because I'm still in dazed)\n" << endl;
+            Sleep(3000); 
+
+        }
       }
 
 
